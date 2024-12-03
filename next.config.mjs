33e90284077f403223bfde45e0dd3f41b0032a90
elikemdanielsdev <elikemdaniels@gmail.com>
2024-12-03 05:23:04 +0000

    General update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cms.elikemdaniels.dev",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,52 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontSize: {
+        "display-1": "clamp(1.75rem, 0.9375rem + 4.0625vw, 5rem)",
+        "display-2": "clamp(1.5rem, 1.25rem + 1.25vw, 2.5rem)",
+        "display-3": "clamp(1.25rem, 1.125rem + 1vw, 2rem)",
+        "sub-display": "clamp(1.125rem, 0.9375rem + 0.9375vw, 1.875rem)",
+      },
+      screens: {
+        "1200": "1200px",
+        "1000": "1000px",
+        "700": "700px",
+        "600": "600px",
+        "500": "500px",
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       mytheme: {
+  //         primary: "#9fcfee",
+  //         secondary: "#5f55ff",
+  //         accent: "#bbc800",
+  //         neutral: "#31293f",
+  //         "base-100": "#fffbf5",
+  //         info: "#0093ff",
+  //         success: "#7edf00",
+  //         warning: "#c86800",
+  //         error: "#be2134",
+  //       },
+  //     },
+  //   ],
+  // },
 };
 export default config;
